e71db0d08bb29fa6f89ceeec85cf7f1d726b065e
Liu Jianping <qq1044559054@gmail.com>
2022-04-13 02:31:52 +1000

    04/13

--- src/state_07.js
+++ src/state_07.js
@@ -1,26 +1,69 @@
 import React, { Component } from 'react'
 import './css/state_07css.css'
+import Center from './state_07/Center'
+import Cinema from './state_07/Cinema'
+import Film from './state_07/Film'
+
+
+
+
 export default class state_07 extends Component {
     state = {
         list:[
             {
             id:1,
-            text:"电影1"
+            text:"电影"
         },{
             id:2,
-            text:"电影2"
+            text:"电影"
         },{
             id:3,
-            text:"电影3"
-        }]
+            text:"电影"
+        }],
+        current:0
+    }
+    which(){
+        switch(this.state.current){
+            case 0:
+                return<Film></Film>
+                case 1:
+                    return<Center></Center>
+                    case 2:
+                        return<Cinema></Cinema>
+
+        }
     }
   render() {
     return (
-      <div><ul>
+      <div>
+        {
+            this.state.current=== 0 && <Film></Film>
+        }
+            
+        {
+            this.state.current=== 1 && <Center></Center>
+        }
+
+        {
+            this.state.current=== 2 && <Cinema></Cinema>
+        }
+        {
+            this.which()
+        }
+          <ul>
           {
-              this.state.list.map(item=>
-                <li key={item.id}>{item.text} </li>)
+              this.state.list.map((item,index)=>
+                <li key={item.id} 
+                className = {this.state.current === index? 'active':'secondaryactive'}
+                onClick={()=>this.handleclick(index)}>{item.text}</li>)
           }</ul></div>
     )
+  }
+  handleclick(index){
+      console.log(index)
+
+      this.setState({
+          current:index
+      })
   }
 }
